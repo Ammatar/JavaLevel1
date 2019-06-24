1d13d5a1828dc9a98f7902884a34d47f52126141
Ammatar <avrezn@gmail.com>
2019-06-25 01:19:56 +0300

    add

--- Lesson8.java
+++ Lesson8.java
@@ -1,6 +1,6 @@
 /**
  * @author Артур Резник,
- * @version Java. Уровень 1 урок 7, 22.06.2019
+ * @version Java. Уровень 1 урок 8, 25.06.2019
  * @testIsPassed https://geekbrains.ru/certificates/606968
  * @gitlink https://github.com/Ammatar
  */
